c1ef6c87667a6a0a187f8d23fbe59998fabf706e
Nagy Máté <nmakaltar@gmail.com>
2021-12-22 12:45:35 +0100

    Add TODOlist, add imba arcane rune.

--- game/scripts/vscripts/GameMode.ts
+++ game/scripts/vscripts/GameMode.ts
@@ -1,6 +1,6 @@
 import { reloadable } from "./lib/tstl-utils";
-import "./modifiers/modifier_panic";
 
+import "./modifiers/modifier_imba_rune_arcane";
 const heroSelectionTime = 10;
 
 declare global {
@@ -12,8 +12,16 @@ declare global {
 @reloadable
 export class GameMode {
     public static Precache(this: void, context: CScriptPrecacheContext) {
-        PrecacheResource("particle", "particles/units/heroes/hero_meepo/meepo_earthbind_projectile_fx.vpcf", context);
-        PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_meepo.vsndevts", context);
+        PrecacheResource(
+            "particle",
+            "particles/units/heroes/hero_meepo/meepo_earthbind_projectile_fx.vpcf",
+            context
+        );
+        PrecacheResource(
+            "soundfile",
+            "soundevents/game_sounds_heroes/game_sounds_meepo.vsndevts",
+            context
+        );
     }
 
     public static Activate(this: void) {
@@ -22,23 +30,40 @@ export class GameMode {
 
     constructor() {
         this.configure();
-        ListenToGameEvent("game_rules_state_change", () => this.OnStateChange(), undefined);
-        ListenToGameEvent("npc_spawned", event => this.OnNpcSpawned(event), undefined);
+        ListenToGameEvent(
+            "game_rules_state_change",
+            () => this.OnStateChange(),
+            undefined
+        );
+        ListenToGameEvent(
+            "npc_spawned",
+            (event) => this.OnNpcSpawned(event),
+            undefined
+        );
     }
 
     private configure(): void {
-        GameRules.SetCustomGameTeamMaxPlayers(DOTATeam_t.DOTA_TEAM_GOODGUYS, 3);
-        GameRules.SetCustomGameTeamMaxPlayers(DOTATeam_t.DOTA_TEAM_BADGUYS, 3);
-
+        // GameRules.SetCustomGameTeamMaxPlayers(DOTATeam_t.DOTA_TEAM_GOODGUYS, 3);
+        // GameRules.SetCustomGameTeamMaxPlayers(DOTATeam_t.DOTA_TEAM_BADGUYS, 3);
+        //GameRules.SetNextRuneSpawnTime(0);
+        //GameRules.G
         GameRules.SetShowcaseTime(0);
         GameRules.SetHeroSelectionTime(heroSelectionTime);
+        const gameMode = GameRules.GetGameModeEntity();
+        gameMode.SetModifierGainedFilter(this.ModifierGainedFilter, {});
+        gameMode.SetRuneEnabled(DOTA_RUNES.DOTA_RUNE_DOUBLEDAMAGE, false);
+        gameMode.SetRuneEnabled(DOTA_RUNES.DOTA_RUNE_ILLUSION, false);
+        gameMode.SetRuneEnabled(DOTA_RUNES.DOTA_RUNE_INVISIBILITY, false);
     }
 
     public OnStateChange(): void {
         const state = GameRules.State_Get();
 
         // Add 4 bots to lobby in tools
-        if (IsInToolsMode() && state == DOTA_GameState.DOTA_GAMERULES_STATE_CUSTOM_GAME_SETUP) {
+        if (
+            IsInToolsMode() &&
+            state == DOTA_GameState.DOTA_GAMERULES_STATE_CUSTOM_GAME_SETUP
+        ) {
             for (let i = 0; i < 4; i++) {
                 Tutorial.AddBot("npc_dota_hero_lina", "", "", false);
             }
@@ -48,6 +73,37 @@ export class GameMode {
         if (state == DOTA_GameState.DOTA_GAMERULES_STATE_PRE_GAME) {
             Timers.CreateTimer(0.2, () => this.StartGame());
         }
+    }
+
+    public ModifierGainedFilter(event: ModifierGainedFilterEvent) {
+        print("Modifier gained filter: " + event.name_const);
+        const duration = event.duration;
+        const target = EntIndexToHScript(
+            event.entindex_parent_const
+        ) as CDOTA_BaseNPC;
+
+        const modifier_name = event.name_const;
+        if (modifier_name === "modifier_rune_arcane") {
+            target.AddNewModifier(
+                target,
+                undefined,
+                "modifier_imba_rune_arcane",
+                { duration: duration }
+            );
+            for (let i = 0; i < 24; ++i) {
+                const ability = target.GetAbilityByIndex(i);
+                if (ability && !ability.IsCooldownReady()) {
+                    ability.EndCooldown();
+                }
+            }
+            for (let i = 0; i < 24; ++i) {
+                const item = target.GetItemInSlot(i);
+                if (item && !item.IsCooldownReady()) {
+                    item.EndCooldown();
+                }
+            }
+        }
+        return false;
     }
 
     private StartGame(): void {
@@ -64,17 +120,16 @@ export class GameMode {
     }
 
     private OnNpcSpawned(event: NpcSpawnedEvent) {
-        // After a hero unit spawns, apply modifier_panic for 8 seconds
-        const unit = EntIndexToHScript(event.entindex) as CDOTA_BaseNPC; // Cast to npc since this is the 'npc_spawned' event
-        if (unit.IsRealHero()) {
-            Timers.CreateTimer(1, () => {
-                unit.AddNewModifier(unit, undefined, "modifier_panic", { duration: 8 });
-            });
-
-            if (!unit.HasAbility("meepo_earthbind_ts_example")) {
-                // Add lua ability to the unit
-                unit.AddAbility("meepo_earthbind_ts_example");
-            }
-        }
+        // // After a hero unit spawns, apply modifier_panic for 8 seconds
+        // const unit = EntIndexToHScript(event.entindex) as CDOTA_BaseNPC; // Cast to npc since this is the 'npc_spawned' event
+        // if (unit.IsRealHero()) {
+        //     Timers.CreateTimer(1, () => {
+        //         unit.AddNewModifier(unit, undefined, "modifier_panic", { duration: 8 });
+        //     });
+        //     if (!unit.HasAbility("meepo_earthbind_ts_example")) {
+        //         // Add lua ability to the unit
+        //         unit.AddAbility("meepo_earthbind_ts_example");
+        //     }
+        // }
     }
 }
---
+++ game/scripts/vscripts/modifiers/modifier_imba_rune_arcane.ts
@@ -0,0 +1,97 @@
+// modifier_imba_rune_arcane = class({})
+
+// function modifier_imba_rune_arcane:IsDebuff()			return false end
+// function modifier_imba_rune_arcane:IsHidden() 			return false end
+// function modifier_imba_rune_arcane:IsPurgable() 		return false end
+// function modifier_imba_rune_arcane:IsPurgeException() 	return false end
+// function modifier_imba_rune_arcane:GetTexture() return "rune_arcane" end
+// function modifier_imba_rune_arcane:GetEffectName() return "particles/generic_gameplay/rune_arcane_owner.vpcf" end
+// function modifier_imba_rune_arcane:GetEffectAttachType() return PATTACH_ABSORIGIN_FOLLOW end
+// function modifier_imba_rune_arcane:DeclareFunctions() return {MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE, MODIFIER_PROPERTY_MANACOST_PERCENTAGE_STACKING, MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE, MODIFIER_EVENT_ON_DEATH} end
+// function modifier_imba_rune_arcane:GetModifierPercentageCooldown() return 50 end
+// function modifier_imba_rune_arcane:GetModifierPercentageManacostStacking() return 50 end
+// function modifier_imba_rune_arcane:GetModifierSpellAmplify_Percentage() return 50 end
+
+// function modifier_imba_rune_arcane:OnDeath(keys)
+// 	if IsServer() and keys.unit == self:GetParent() then
+// 		self:Destroy()
+// 	end
+// end
+import { BaseModifier, registerModifier } from "../lib/dota_ts_adapter";
+
+@registerModifier()
+export class modifier_imba_rune_arcane extends BaseModifier {
+    IsDebuff(): boolean {
+        return false;
+    }
+    IsHidden(): boolean {
+        return false;
+    }
+    IsPurgable(): boolean {
+        return false;
+    }
+    IsPurgeException(): boolean {
+        return false;
+    }
+    GetTexture(): string {
+        return "rune_arcane";
+    }
+    GetEffectName(): string {
+        return "particles/generic_gameplay/rune_arcane_owner.vpcf";
+    }
+    GetEffectAttachType(): ParticleAttachment_t {
+        return ParticleAttachment_t.PATTACH_ABSORIGIN_FOLLOW;
+    }
+    DeclareFunctions(): modifierfunction[] {
+        return [
+            modifierfunction.MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
+            modifierfunction.MODIFIER_PROPERTY_MANACOST_PERCENTAGE_STACKING,
+            modifierfunction.MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
+            // Take this out and it lasts through death
+            modifierfunction.MODIFIER_EVENT_ON_DEATH,
+        ];
+    }
+    GetModifierPercentageCooldown(): number {
+        print("nonstacking");
+        return 50;
+    }
+
+    GetModifierPercentageCooldownStacking(): number {
+        print("stacking");
+        return 50;
+    }
+    GetModifierPercentageManacostStacking(): number {
+        return 50;
+    }
+    GetModifierSpellAmplify_Percentage(): number {
+        return 50;
+    }
+
+    OnDeath(event: ModifierAttackEvent): void {
+        if (IsServer() && event.unit == this.GetParent()) {
+            this.Destroy();
+        }
+    }
+
+    // // Override speed given by Modifier_Speed
+    // GetModifierMoveSpeed_Absolute(): number {
+    //     return 540;
+    // }
+
+    // // Run when modifier instance is created
+    // OnCreated(): void {
+    //     if (IsServer()) {
+    //         // Think every 0.3 seconds
+    //         this.StartIntervalThink(0.3);
+    //     }
+    // }
+
+    // // Called when intervalThink is triggered
+    // OnIntervalThink(): void {
+    //     const parent = this.GetParent();
+
+    //     parent.MoveToPosition(
+    //         (parent.GetAbsOrigin() + RandomVector(400)) as Vector
+    //     );
+    // }
+}
